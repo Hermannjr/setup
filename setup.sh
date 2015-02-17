c81cac4586908830b1d7eb35a235c2714fbe70d9
Hermannjr <hermannjr@gmail.com>
2015-02-17 17:02:03 +0100

    Added SPF13 which will be installed from a fork. Removed dotfiles repo reference for now

--- setup.sh
+++ setup.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
-# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
-# for headless setup. 
-
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
+# install git-core first if it's not yet installed
 sudo apt-get install -y git-core
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+# run SPF13-VIM shell script
+curl https://raw.githubusercontent.com/Hermannjr/spf13-vim/3.0/bootstrap.sh -L -o - | sh
 
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-npm install -g jshint
-
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
-
-# git pull and install dotfiles as well
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-git clone https://github.com/Hermannjr/dotfiles.git
-ln -sb dotfiles/.tmux conf .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sb dotfiles/.vimrc
-ln -sd .vim dotfiles/.vim
+echo colorscheme ir_black >> .vimrc.local
